0cefcaa995398b87aecdc18757983f2d3085913e
parvez <parvez@booleanminds.com>
2017-08-22 23:15:32 +0530

    corrected test cases

--- src/test/java/com/mslc/TestHelloWorld.java
+++ src/test/java/com/mslc/TestHelloWorld.java
@@ -27,6 +27,6 @@ public class TestHelloWorld {
 	   {
 	      h.setName("World");
 	      assertEquals(h.getName(),"World");
-	      assertEquals(h.getMessage(),"Updated Hello World!");
+	      assertEquals(h.getMessage(),"Hello World!");
 	   }
 }
